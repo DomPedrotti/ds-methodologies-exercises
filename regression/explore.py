3e121baa683796c7bbe6bfb3bef34cf0ef46a58f
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-10-11 09:50:17 -0500

    first commit of exploration functions

--- regression/explore.py
+++ regression/explore.py
@@ -1,1 +1,22 @@
 # exploration method functions
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import wrangle as wgl
+import split_scale as ss 
+
+def plot_variable_pairs(dataframe):
+    
+    pass
+
+
+def months_to_years(tenure_months, df):
+    pass
+
+
+
+def plot_categorical_and_continous_vars(categorical_var, continuous_var, df):
+    pass
