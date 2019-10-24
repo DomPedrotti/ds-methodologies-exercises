e58b50372f7a51a9ea9498c719a3be995a4785f0
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-10-24 14:34:45 -0500

    added doc strings

--- classification/explore.py
+++ classification/explore.py
@@ -5,14 +5,32 @@ import seaborn as sns
 
 
 def plot_bars(features, target, df):
-    for column in df[features].select_dtypes([object, int]).columns.tolist():
+    '''
+    plot_bars(features, target, df):
+
+    - iterates through dataframe columns and plots barcharts for all categorical columns
+
+    args:
+        features: list of string column names
+        target: name of target variable column
+        df: data frame containing feature and target columns
+
+    '''
+
+    # iterate through feature columns and select columns of object or integer type
+    for column in df[features].select_dtypes([object, int]).columns.tolist():\
+        # check how many unique values column has, if over 5, we won't use column
         if len(df[column].unique()) <= 5:
+            # build chart
             sns.barplot(column, target, data=df)
             plt.title(column)
             plt.ylabel(target)
             plt.show()
 
 def plot_violin(features, target, df):
+    '''
+    
+    '''
     for descrete in df[features].select_dtypes([object,int]).columns.tolist():
         if df[descrete].nunique() <= 5:
             for continous in df[features].select_dtypes(float).columns.tolist():
