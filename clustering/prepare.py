7555add0fb56c509d1f2daa542292eeeefad286c
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-11-01 15:47:22 -0500

    two funtions for data prep so far

---
+++ clustering/prepare.py
@@ -0,0 +1,19 @@
+from summarize import nulls_by_col
+
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    missing_columns = nulls_by_col(df)
+    features = missing_columns[missing_columns.pct_rows_missing < (1-prop_required_column)].index.tolist()
+    
+    df = df[features]
+
+    thresh = int(df.shape[1]*prop_required_row) + 1
+    df = df.dropna( thresh = thresh )
+    
+    return df
+
+def get_single_unit_properties(df):
+
+    df = (df[(df.propertylandusedesc == 'Single Family Residential') |
+             (df.propertylandusedesc == 'Manufactured, Modular, Prefabricated Homes')])
+
+    return df
