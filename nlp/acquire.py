0b5d53d81b92b3e003be5aa47396619a34cda5ca
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-12-06 09:24:12 -0600

    slight changes after Ryans instructions

--- nlp/acquire.py
+++ nlp/acquire.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
-import csv
 from os import path
 
 def acquire_news_articles(from_cache = True):
@@ -36,3 +35,24 @@ def acquire_news_articles(from_cache = True):
             news_df = news_df.append(category, ignore_index=True)
         news_df.to_json('news_articles.json',orient='records')
         return news_df
+
+def get_blog_articles(from_cache = True):
+    if from_cache == True and path.exists('blog_posts.json'):
+        return pd.read_json('blog_posts.json', orient='records')
+    else:
+        articles = [
+            'https://codeup.com/codeups-data-science-career-accelerator-is-here/',
+            'https://codeup.com/data-science-myths/',
+            'https://codeup.com/data-science-vs-data-analytics-whats-the-difference/','https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/',
+            'https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/']
+        headers = {'User-Agent': 'Codeup Data Science Student'}
+        data = []
+        for article in articles:
+            response = get(article,headers = headers)
+            soup = BeautifulSoup(response.text)
+            title = soup.title.get_text()
+            content = soup.select_one('div.mk-single-content.clearfix').get_text()
+            data.append({'title': title, 'content': content})   
+
+    pd.DataFrame(data).to_json('blog_posts.json', orient = 'records')
+    return pd.DataFrame(data)
